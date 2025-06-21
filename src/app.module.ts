ae269263a4d354d25cac8edc0980920d93c03346
feral <feral.x@yandex.ru>
2025-06-21 18:02:25 +1100

    "add modules"

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,14 @@ import { PrismaModule } from './core/prisma/prisma.module';
 import { AuthModule } from './core/auth/auth.module';
 import { UserModule } from './core/user/user.module';
 import {ConfigModule} from "@nestjs/config";
+import { ProductModule } from './core/product/product.module';
+import { CategoryModule } from './core/category/category.module';
+import { ColorModule } from './core/color/color.module';
+import { ReviewModule } from './core/review/review.module';
+import { OrderModule } from './core/order/order.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({isGlobal: true}),PrismaModule, AuthModule, UserModule],
+  imports: [ConfigModule.forRoot({isGlobal: true}),PrismaModule, AuthModule, UserModule, ProductModule, CategoryModule, ColorModule, ReviewModule, OrderModule],
   controllers: [],
   providers: [],
 })
